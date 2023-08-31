951ddd62357a29fad54dcf9ba4d54a6b647a0bda
petit-pierre <aubree.pierre2@gmail.com>
2023-08-31 17:37:27 +0200

    V0.1.1 remplacement des elements html par ceux generé en javascript

--- FrontEnd/assets/frontjs.js
+++ FrontEnd/assets/frontjs.js
@@ -4,5 +4,15 @@ const works = await worksGet.json();
 for (let i=0; i<works.length; i++) {
     const titre = works[i].title
     const image = works[i].imageUrl
+    let figure = `
+        <figure>
+            <img src="${image}" alt="${titre}">
+            <figcaption>${titre}</figcaption>
+        </figure>
+        `
+
+    let gallery = document.querySelector(".gallery")
+    gallery.innerHTML += figure
+    
     console.log (titre,image);
 }
