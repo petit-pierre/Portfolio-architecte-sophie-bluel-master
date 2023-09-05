89bffc0e55aae654d79bc18c55a35607fe879dac
petit-pierre <aubree.pierre2@gmail.com>
2023-09-05 10:28:00 +0200

    V0.2.6 login.js a fixer

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,34 +1,52 @@
+let email;
+let password;
+let response = "test";
+
 async function post() {
-  const post = await fetch("http://localhost:5678/users/login", {
+  let formData = new FormData();
+  formData.append("email", email.value);
+  formData.append("password", password.value);
+  console.log(formData);
+  const post = await fetch("http://localhost:5678/api/users/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json;charset=utf-8",
     },
-    body: JSON.stringify(mail, pass),
+    body: JSON.stringify(formData),
   });
+
   let result = await response.json();
   alert(result.message);
   console.log(result.message);
-  console.log(mail.value);
-  console.log(pass.value);
 }
 
 submit();
 
-function email() {
-  let mail = document.getElementById("email");
-  return mail.value;
+function emailListen() {
+  email = document.getElementById("email");
+  return email.value;
 }
 
-function password() {
-  let pass = document.getElementById("name");
-  return pass.value;
+function passwordListen() {
+  password = document.getElementById("name");
+  return password.value;
 }
 
 async function submit() {
   let form = document.getElementById("submit");
   form.addEventListener(/*"submit"*/ "click", (event) => {
     event.preventDefault();
+    emailListen();
+    let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+");
+    if (emailRegExp.test(email.value)) {
+      console.log("mail ok");
+    } else {
+      console.log("ceci n'est pas un mail");
+    }
+    passwordListen();
+    if (password.value === "") {
+      console.log("le mot de passe est vide");
+    }
     post();
   });
 }
